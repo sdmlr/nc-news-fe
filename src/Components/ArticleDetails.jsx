b5c88c7e76f4ac33a5014df92ba9a86f28bbb9b4
sdmlr <sdmlr_@hotmail.com>
2024-11-05 17:40:05 +0000

    vote on article with posibility of switching added

--- src/Components/ArticleDetails.jsx
+++ src/Components/ArticleDetails.jsx
@@ -7,6 +7,9 @@ function ArticleDetails() {
   const { article_id } = useParams();
   const [article, setArticle] = useState([]);
   const [comments, setComments] = useState([]);
+  const [voteCount, setVoteCount] = useState(article.votes);
+  const [hasUpvoted, setHasUpvoted] = useState(false);
+  const [hasDownvoted, setHasDownvoted] = useState(false);
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -16,6 +19,7 @@ function ArticleDetails() {
       .get(`https://news-project-fe7s.onrender.com/api/articles/${article_id}`)
       .then((response) => {
         setArticle(response.data.article);
+        setVoteCount(response.data.article.votes);
         setIsLoading(false);
         setIsError(false);
       })
@@ -41,6 +45,37 @@ function ArticleDetails() {
   if (isError) return <p>Error!</p>;
   if (!article) return null;
 
+  const updateVote = (incVote) => {
+    const originalCount = voteCount;
+
+    setVoteCount((prevCount) => prevCount + incVote);
+
+    axios
+      .patch(
+        `https://news-project-fe7s.onrender.com/api/articles/${article_id}`,
+        { inc_votes: incVote }
+      )
+      .catch((err) => {
+        setVoteCount(originalCount);
+      });
+  };
+
+  const handleUpvote = () => {
+    if (!hasUpvoted) {
+      setHasUpvoted(true);
+      setHasDownvoted(false);
+      updateVote(hasDownvoted ? 2 : 1);
+    }
+  };
+
+  const handleDownvote = () => {
+    if (!hasDownvoted) {
+      setHasDownvoted(true);
+      setHasUpvoted(false);
+      updateVote(hasUpvoted ? -2 : -1);
+    }
+  };
+
   return (
     <div className="article-details">
       <h2>{article.title}</h2>
@@ -49,6 +84,15 @@ function ArticleDetails() {
       <p>Created at: {new Date(article.created_at).toLocaleDateString()}</p>
       <p>{article.body}</p>
 
+      <div className="vote-section">
+        <p className="vote-count">Votes: {voteCount}</p>
+        <button onClick={handleUpvote} disabled={hasUpvoted}>
+          +
+        </button>
+        <button onClick={handleDownvote} disabled={hasDownvoted}>
+          -
+        </button>
+      </div>
       <h3>Comments</h3>
       <p>Comments: {article.comment_count}</p>
       <div className="comments-section">
