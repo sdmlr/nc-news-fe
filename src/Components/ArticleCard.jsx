cfcf3acfa190da5d41c0127ed22a14513b73379e
sdmlr <sdmlr_@hotmail.com>
2024-11-06 15:02:38 +0000

    postComment functionality added

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,23 +1,25 @@
 import { Link } from "react-router-dom";
 
-function ArticleCard({ articles = [] }) {
+function ArticleCard({ article }) {
   return (
     <>
       <ul className="article-list">
-        {articles.map((article) => (
-          <li key={article.article_id} className="article-card">
-            <Link to={`/articles/${article.article_id}`} className="article-link">
-              <img
-                src={article.article_img_url}
-                alt="article image"
-                className="article-img"
-              />
-              <div className="article-content">
-                <h3>{article.title}</h3>
-              </div>
-            </Link>
-          </li>
-        ))}
+        <div className="article-card">
+          <Link to={`/articles/${article.article_id}`} className="article-link">
+            <img
+              src={article.article_img_url}
+              alt="article image"
+              className="article-img"
+            />
+            <div className="article-content">
+              <h3>{article.title}</h3>
+              <p>{new Date(article.created_at).toLocaleDateString()}</p>
+              <p>Author: {article.author}</p>
+              <p>Topic: {article.topic}</p>
+              <p>Comments: {article.comment_count}</p>
+            </div>
+          </Link>
+        </div>
       </ul>
     </>
   );
